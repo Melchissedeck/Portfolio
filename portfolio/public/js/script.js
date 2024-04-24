94a86938cdfaf9c9f6d3a81e219b0b7fd3e43e04
Melchissedeck AFOUDAH <afoudahmelchissedeck@gmail.com>
2024-04-24 16:15:05 +0200

    Ajout des instructions Git

--- portfolio/public/js/script.js
+++
@@ -1,6 +0,0 @@
-// Pour cacher la page du dessus
-setTimeout(function() {
-    var overlay = document.getElementById('overlay');
-    overlay.classList.add('hidden');
-}, 1000); // 1000 millisecondes = 1 secondes
-
